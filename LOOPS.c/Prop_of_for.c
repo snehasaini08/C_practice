4732564551ef73d2ebba8faf12044fcfd8f01968
Sneha <snehasaini143786@gmail.com>
2025-11-16 23:59:06 -0200

    Practice more

--- LOOPS.c/Prop_of_for.c
+++ LOOPS.c/Prop_of_for.c
@@ -42,7 +42,7 @@
     }
 }
 */
-int main()
+/*int main()
 {
     int i,j;
     for(i=1,j=0;i<5,j<3;i++,j++)//without j++ it was going to an infinite loop
@@ -50,6 +50,69 @@ int main()
         printf("%d%d\n",i,j);
     }
     return 0;
+}*/
+//or
+/*int main()
+{
+    int i,j;
+    for(i=1,j=0;i<=100,j<3;i++,j++)//without j++ it was going to an infinite loop
+    {
+        printf("%d%d\n",i,j);
+    }
+    return 0;
+}*/
+
+ /*int main()
+{
+    int i,j;
+    for(i=1,j=0;i<=100 && j<3;i++,j++)
+    {
+        printf("%d%d\n",i,j);
+    }
+    return 0;
+}*/
+
+
+/*int main()
+{
+    int i,j;
+    for(i=1,j=0;i<=100 || j<3;i++,j++)    //doubt in output
+    {
+        printf("%d%d\n",i,j);
+    }
+    return 0;
+}*/
+//Expression 3
+/*int main()
+{
+    int i,j;
+    for(i=1,j=0;i<=100 , j<3;)    //doubt in output
+    {
+        printf("%d%d\n",i,j);
+        i++;
+        j++;
+    }
+    return 0;
+}*/
+
+//2. if only uses i++ not j++
+
+int main()
+{
+    int i,j;
+    for(i=1,j=0;i<=100 , j<3;)    //doubt in output
+    {
+        printf("%d%d\n",i,j);  //loop going to infinite in this case but if doing j++ output is according to my dry run
+        
+        j++;
+    }
+    return 0;
 }
+
+
+
+
+
+
 
 
