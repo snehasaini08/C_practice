08bb8daa02b1b753f4c8882fb72e77851990f8de
Sneha <snehasaini143786@gmail.com>
2025-11-18 16:07:14 -0200

    My change as learning break

--- LOOPS.c/break.c
+++ LOOPS.c/break.c
@@ -1,6 +1,5 @@
-#include<stdio.h>
-int main()
-{
+
+/*{
     int a,i,sum=0;
     for(i=1;i<=5;i++)
     {
@@ -14,6 +13,20 @@ int main()
     }
     printf("sum is %d",sum);
     return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+    int a,sum=0;
+    while(1)
+    {
+        printf("Enter a number: ");
+        scanf("%d",&a);
+        if(a<0)
+        break;
+        sum=sum+a;
+    }
+    printf("Sum=%d",sum);
 }
 
 
