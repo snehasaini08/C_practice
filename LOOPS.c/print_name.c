c658a6eb37141aa0a6c083e8dec55848e5919b37
Sneha <snehasaini143786@gmail.com>
2025-11-18 23:35:18 -0200

    Today codes

---
+++ LOOPS.c/print_name.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/*int main()
+{
+    int i=1;
+    for(i=1;i<=10;i++)
+    {
+        printf("Sneha\n");
+    }
+}*/
+
+int main()
+{
+    int i=1;
+    do
+    {
+        printf("%d\n",i);
+        i++;
+    }while(i<=10);
+    printf("End of the program");   
+}
