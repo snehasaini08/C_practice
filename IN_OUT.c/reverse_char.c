b22667565793266582a222a0c8fb99c104e63f39
Sneha <snehasaini143786@gmail.com>
2025-12-01 22:43:31 -0200

    reverse char

---
+++ IN_OUT.c/reverse_char.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    //declare and initialize reversed character 
+    char char1 = 'X';
+    char char2 = 'Y';
+    char char3 = 'Z';
+    //print the original and reversed charater 
+    printf("the reverse of the charater %c %c %c is %c %c %c\n ", char1, char2, char3, char3, char2, char1 );
+    return 0;
+}
