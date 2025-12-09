f436d9e3cf20310f95dda92450a2f4a3b02570aa
Sneha <snehasaini143786@gmail.com>
2025-12-08 22:33:07 -0200

    .

--- POINTERS.c/2_pointer.c
+++ POINTERS.c/2_pointer.c
@@ -10,13 +10,23 @@ int main()
 
 }*/
 
-{
+/*{
     int a=10,b=5;
     int *p,*q;
     p=&a;
     //q=&b;   // her in my case without q=&b does not giving error
     *q=*p;
     printf("a= %d %d %d\n",a,*p,*q);
+    return 0;
+
+}*/
+{
+    //int a=10,b=5;
+    float a=10,b=5;
+    float *p,*q;  // a and b should also be of same datatype
+    p=&a;
+    q=&b;
+    printf("a=%f %f %f\n",a,*p,*q);
     return 0;
 
 }
