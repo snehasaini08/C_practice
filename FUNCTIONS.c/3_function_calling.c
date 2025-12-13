e72970ec953d5dce0dbfefda5169b3276e6a8c4d
Sneha <snehasaini143786@gmail.com>
2025-12-13 15:25:49 -0200

    function calling

---
+++ FUNCTIONS.c/3_function_calling.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+//call by value
+/*void fun(int ,int);
+void main()
+{
+    int x=5,y=7;
+    fun(x,y);
+    printf("Inside main(calling function): \n");
+    printf("x=%d y=%d\n",x,y);
+
+}
+void fun(int x,int y)
+{
+    x=7;
+    y=5;
+    printf("Inside fun(called function):\n ");
+    printf("x=%d y=%d\n",x,y );
+}*/
+
+
+//call by reference
+void fun(int* ,int*);
+void main()
+{
+    int x=5,y=7;
+    fun(&x,&y);
+    printf("Inside main(calling function): \n");
+    printf("x=%d y=%d\n",x,y);
+
+}
+void fun(int* x,int* y)
+{
+    *x=7;
+    *y=5;
+    printf("Inside fun(called function):\n ");
+    printf("x=%d y=%d\n",*x,*y );
+}
