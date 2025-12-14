c5d6d5dc7a15193261a31b6f1a61282744977308
Sneha <snehasaini143786@gmail.com>
2025-12-14 12:20:38 -0200

    functions

---
+++ FUNCTIONS.c/5_no_arg_with_ret.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/*int sum(void);
+void main()
+{
+    int s;
+    s=sum();
+    printf("Sum=%d",s);
+
+}
+int sum()
+{
+    int a=5,b=7,sum=0;
+    sum=a+b;
+    return sum,a,b;
+    //printf("Hi");
+}*/
+
+char sum(void);
+void main()
+{
+    char s;
+    s=sum();
+    printf("Sum=%c",s);
+
+}
+char sum()
+{
+    int a=5,b=7,sum=0;
+    sum=a+b;
+    return 'a','0','i'  ;
+    return sum;
+    //printf("Hi");
+}
+
