7da8f64250a09a7baa5458ea72d8100269c51bb7
Sneha <snehasaini143786@gmail.com>
2025-12-21 23:06:04 -0200

    pattern

--- LOOPS.c/PATTERN.c/3_pattern.c
+++ LOOPS.c/PATTERN.c/3_pattern.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,nst;
+    int n,i,j,nsp,nst;
     printf("Enter number of rows: ");
     scanf("%d",&n);
+    nsp=n-1;
     nst=1;
     for(i=1;i<=n;i++)
     {
+        for(j=1;j<=nsp;j++)
+        {
+            printf(" ");
+        }
         for(j=1;j<=nst;j++)
         {
             printf("*");
         }
         printf("\n");
+        nsp=nsp-1;
         nst=nst+1;
     }
-    return 0;
 }
