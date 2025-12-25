de7b01eb9be7d34301e443138e009670cf93b20e
Sneha <snehasaini143786@gmail.com>
2025-12-24 23:04:17 -0200

    .

---
+++ practice.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+   /*long int a=32777777;
+   printf("%d\n",a);
+   printf("%u\n",a);
+   //printf("%zu", sizeof(unsigned long int));
+
+
+    //printf("%d", sizeof(char));
+    return 0;*/
+   /* float a=10;
+    float b=7;
+    printf("%f\n",a+b);
+    printf("%f\n",a-b);
+    printf("%f\n",a*b);
+    printf("%f\n",a/b);
+   // printf("%f\n",a%b);
+    return 0;*/
+    /*int y, x=10;
+    y=x--;
+    y=x;
+    x=x-1;
+    printf("%d\n",x);
+    printf("%d",y);
+    return 0;*/
+    int  a=5,b,c,d;
+    b=++a;
+    c=a++;
+    d=++a;
+    printf("%d %d %d %d\n",a,b,c,d);
+    return 0;
+}
