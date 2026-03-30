e1f966fca442d59986316583c5e8da7bced54928
Sneha <snehasaini143786@gmail.com>
2026-03-30 12:42:57 +0530

    commit changes

--- ARRAY.c/8_2_2d_arr_mul.c
+++ ARRAY.c/8_2_2d_arr_mul.c
@@ -1,42 +1,81 @@
 #include<stdio.h>
+#define N 50
 int main()
 {
-    int a[3][3],b[3][3],i,j;
-    printf("Enter 2D array elements of first array: ");
-    for(i=0;i<3;i++)
+    int a[N][N],b[N][N],c[N][N],i,j,k,sum,m,n,p,q;
+    printf("Enter rows and colums of first matrix: \n");
+    scanf("%d %d",&m,&n);
+
+    printf("Input is : ");
+    for(i=0;i<m;i++)
     {
-        for(j=0;j<3;j++)
+        for(j=0;j<n;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
-    printf("Matrix is:\n");
-    for(i=0;i<3;i++)
+
+    printf("Enter rows and columns of second matrix:\n");
+    scanf("%d %d",&p,&q);
+
+    printf("Input is : ");
+    for(i=0;i<p;i++)
     {
-        for(j=0;j<3;j++)
-        {
-            printf("%d\t",a[i][j]);
-        }
-        printf("\n");
-    }
-    printf("Enter 2D array elements of second array:  ");
-    for(i=0;i<3;i++)
-    {
-        for(j=0;j<3;j++)
+        for(j=0;j<q;j++)
         {
             scanf("%d",&b[i][j]);
         }
     }
-    printf("Matrix is:\n");
-    for(i=0;i<3;i++)
+
+    printf("\nFirst matrix is :\n  ");
+    for(i=0;i<m;i++)
     {
-        for(j=0;j<3;j++)
+        for(j=0;j<n;j++)
         {
             printf("%d\t",b[i][j]);
         }
         printf("\n");
     }
-    
+
+     printf("\nSecond matrix is :\n  ");
+    for(i=0;i<p;i++)
+    {
+        for(j=0;j<q;j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+    //As we know the for multiplication ( col of 1st matrix{n} = row of second matrix{p}
+    if(n!=p)
+    {
+        printf("matrix cannot multiply");
+    }
+    else{
+        for(i=0;i<m;i++) //m=no of rows of 1st matrix
+        {
+            for(j=0;j<q;j++) //q=no of col of 2nd mat
+            {
+                sum=0;
+                for(k=0;k<n;k++)
+                {
+                    sum=sum+(a[i][k]*b[k][j]);
+                }
+                c[i][j]=sum;
+                //scanf("%d",&a[i][j])
+            }
+        }
+        printf("Multiplication is: \n");
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<q;j++)
+            {
+                printf("%d\t",c[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
     return 0;
     
 }
